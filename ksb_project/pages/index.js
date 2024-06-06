384413a6670dc4c8867fe66966650cfa827883bd
sachin <sachinacharya901@gmail.com>
2024-06-06 11:40:34 +0545

    header added

--- ksb_project/pages/index.js
+++ ksb_project/pages/index.js
@@ -1,17 +1,30 @@
-
-
-
-
-
-
+import { useEffect, useState } from "react";
+import Header from "./Components/Header";
 
 export default function Home() {
+  let [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("/api/category")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log(categories, "Categories")
+
   return (
 
 
+    <div>
 
-    <h3>
-      Hello
-    </h3>
+
+      <Header />
+
+      <h3>
+        Hello
+      </h3>
+
+    </div>
   );
 }
