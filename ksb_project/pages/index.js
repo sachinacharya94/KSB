068469e7e2826c47130d0ba175d13b68f87effd8
sachin <sachinacharya901@gmail.com>
2024-06-06 10:48:04 +0545

    backend connected

--- ksb_project/pages/index.js
+++ ksb_project/pages/index.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+
+
+
+
+
 
 export default function Home() {
   return (
+
+
+
     <h3>
       Hello
     </h3>
