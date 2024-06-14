a574f82f1c91f2ca88f72c314982b2e96a128cd2
NishanThapa10 <107379632+NishanThapa10@users.noreply.github.com>
2024-06-14 14:07:53 +0545

    user token tried

--- ksb_project/pages/api/user.js
+++ ksb_project/pages/api/user.js
@@ -2,57 +2,92 @@ import '../../Database/connection'
 const User = require('../../Models/userModel')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const Token = require('../../Models/tokenModel')
+const jwt = require('jsonwebtoken')
+const {expressjwt} = require('express-jwt')
+const crypto = require('crypto')
 
 
-export default async function handler(req,res){
-try {
-  if(req.method=="POST"){
+export default async function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const { username, email, password, token } = req.body
 
-    let {username, email, password} = req.body
+      if (username && email && password) {
+        let userExists = await User.findOne({ username })
+        if (userExists) {
+          return res.status(400).json({ error: "Username not available." })
+        }
 
-    if(username && email && password) {
-      let userExists = await User.findOne({username})
-    if(userExists){
-      return res.status(400).json({error:"Username not available."})
-    }
-    userExists = await User.findOne({email})
-    if(userExists){
-      return res.status(400).json({error:"Email already registered"})
-    }
-    let salt = await bcrypt.genSalt(saltRounds)
-    let hashed_password = await bcrypt.hash(password,salt)
+        userExists = await User.findOne({ email })
+        if (userExists) {
+          return res.status(400).json({ error: "Email already registered" })
+        }
 
-    let newUser = await User.create({
-      username,
-      email,
-      password: hashed_password
-    })
-    if(!newUser){
-      return res.status(400).json({error:"Something went wrong"})
-    }
-    res.send(newUser)
-    }
-    if(email && password){
-      let user = await User.findOne({email})
-      if(!user){
-        return res.status(400).json({error:"Email not registered"})
+        let salt = await bcrypt.genSalt(saltRounds);
+        let hashed_password = await bcrypt.hash(password, salt)
+
+        let newUser = await User.create({
+          username,
+          email,
+          password: hashed_password
+        });
+
+        if (!newUser) {
+          return res.status(400).json({ error: "Something went wrong" })
+        }
+
+        let token = await Token.create({
+          token: crypto.randomBytes(16).toString('hex'),
+          user: newUser._id
+        });
+
+        if (!token) {
+          return res.status(400).json({ error: "Something went wrong" })
+        }
+
+        res.send(newUser)
       }
-      const validPassword = await bcrypt.compare(password,user.password)
-      if(!validPassword){
-        return res.status(400).json({error:"Invalid Password or Password does not match"})
+
+
+       if (email && password) {
+        let user = await User.findOne({ email })
+
+        if (!user) {
+          return res.status(400).json({ error: "Email not registered" })
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password)
+
+        if (!validPassword) {
+          return res.status(400).json({ error: "Invalid Password or Password does not match" })
+        }
+
+        const { _id, username } = user
+
+        let token = jwt.sign({
+          _id,
+          username
+        }, process.env.JWT_SECRET)
+
+        res.cookie('myCookie', token, { expire: 86400 })
+        res.send({ token, user: { _id, username } })
+      } else {
+        return res.status(400).json({ error: "Invalid request parameters." })
       }
-      res.send("Signin successfull",user)
+    } else if (req.method === "GET") {
+      let users = await User.find()
+
+      if (!users) {
+        return res.status(400).json({ error: "Something went wrong" })
+      }
+
+      res.send(users);
+    } else {
+      res.status(405).json({ error: "Method not allowed" })
     }
-    else {
-      return res.status(400).json({ error: "Invalid request parameters." })
+  } catch (error) {
+    console.error("Error in handler:", error);
+    res.status(500).json({ error: "Internal server error" })
   }
-    
-  }
-  else {
-    res.status(405).json({ error: "Method not allowed" })
 }
-  
-} catch (error) {
-  res.status(500).json({ error: "Internal server error" })
-}
-} 
