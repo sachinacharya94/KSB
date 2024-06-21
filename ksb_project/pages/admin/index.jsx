8e070eb52358c0372f376df453932b2f12cf1759
sachin <sachinacharya901@gmail.com>
2024-06-21 15:23:59 +0545

    frontend issue solved

--- ksb_project/pages/admin/index.jsx
+++ ksb_project/pages/admin/index.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
+import { authenticate, login } from '../api/userAPI'
+import Swal from 'sweetalert2'
 
 const index = () => {
+    let [error, setError] = useState("")
+    let [success, setSuccess] = useState(false)
     let [user, setUser] = useState({
-        username: "",
+
         email: "",
         password: ""
     })
@@ -13,20 +17,36 @@ const index = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        login(user)
+            .then(data => {
+                if (data && data.error) {
+                    console.log(data, "login")
+                    setError(data.error)
+                }
+                else {
+                    setSuccess(true)
+                    authenticate(data)
+                }
+                console.log(data, "DATA")
+            })
 
 
     }
-    console.log(user, "USERRRRRR")
+
+    const showError = () => {
+        if (error) {
+            Swal.fire("Error", error, "error")
+            setError("")
+        }
+    }
+    // console.log(user, "USERRRRRR")
 
     return (
         <div className='mt-20 '>
-
+            {showError()}
 
             <form className="max-w-sm mx-auto border-[1px] border-solid border-gray-600 p-7 rounded-lg">
-                <div className="mb-5">
-                    <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User Name</label>
-                    <input type="text" id="name" name='username' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Name" required onChange={handleChange} />
-                </div>
+
                 <div className="mb-5">
                     <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                     <input type="email" id="email" name='email' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required onChange={handleChange} />
--- ksb_project/pages/api/user.js
+++ ksb_project/pages/api/user.js
@@ -4,98 +4,99 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 const Token = require('../../Models/tokenModel')
 const jwt = require('jsonwebtoken')
-const {expressjwt} = require('express-jwt')
+const { expressjwt } = require('express-jwt')
 const crypto = require('crypto')
 import cookie from 'cookie'
 
 export default async function handler(req, res) {
-  
-    if (req.method === "POST") {
-      const { username, email, password } = req.body
 
-      if (username && email && password) {
-        let userExists = await User.findOne({ username })
-        if (userExists) {
-          return res.status(400).json({ error: "Username not available." })
-        }
+  if (req.method === "POST") {
+    const { username, email, password } = req.body
 
-        userExists = await User.findOne({ email })
-        if (userExists) {
-          return res.status(400).json({ error: "Email already registered" })
-        }
-
-        let salt = await bcrypt.genSalt(saltRounds);
-        let hashed_password = await bcrypt.hash(password, salt)
-
-        let newUser = await User.create({
-          username,
-          email,
-          password: hashed_password
-        });
-
-        if (!newUser) {
-          return res.status(400).json({ error: "Something went wrong" })
-        }
-
-        let token = await Token.create({
-          token: crypto.randomBytes(16).toString('hex'),
-          user: newUser._id
-        });
-
-        if (!token) {
-          return res.status(400).json({ error: "Something went wrong" })
-        }
-
-        res.send(newUser)
+    if (username && email && password) {
+      let userExists = await User.findOne({ username })
+      if (userExists) {
+        return res.status(400).json({ error: "Username not available." })
       }
 
-
-      else if (email && password){
-        let user = await User.findOne({ email })
-
-        if (!user) {
-          return res.status(400).json({ error: "Email not registered" })
-        }
-
-        const validPassword = await bcrypt.compare(password, user.password)
-
-        if (!validPassword) {
-          return res.status(400).json({ error: "Invalid Password or Password does not match" })
-        }
-
-        const { _id, username } = user
-
-        let token = jwt.sign({
-          _id,
-          email,
-          username
-        }, process.env.JWT_SECRET)
-
-
-        res.setHeader('Set-Cookie', cookie.serialize('myCookie', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== 'development',
-          maxAge: 86400,
-          sameSite: 'strict',
-          path: '/'
-        }));
-
-        // res.cookie('myCookie', token, { expire: 86400 })
-        res.send({ token, user: { _id, email, username } })
-      } else {
-        return res.status(400).json({ error: "Invalid request parameters." })
+      userExists = await User.findOne({ email })
+      if (userExists) {
+        return res.status(400).json({ error: "Email already registered" })
       }
-    } else if (req.method === "GET") {
-      let users = await User.find()
 
-      if (!users) {
+      let salt = await bcrypt.genSalt(saltRounds);
+      let hashed_password = await bcrypt.hash(password, salt)
+
+      let newUser = await User.create({
+        username,
+        email,
+        password: hashed_password
+      });
+
+      if (!newUser) {
         return res.status(400).json({ error: "Something went wrong" })
       }
 
-      res.send(users);
-    } else {
-      res.status(405).json({ error: "Method not allowed" })
+      let token = await Token.create({
+        token: crypto.randomBytes(16).toString('hex'),
+        user: newUser._id
+      });
+
+      if (!token) {
+        return res.status(400).json({ error: "Something went wrong" })
+      }
+
+      res.send(newUser)
     }
+
+
+    else if (email && password) {
+      let user = await User.findOne({ email })
+
+      if (!user) {
+        return res.status(400).json({ error: "Email not registered" })
+      }
+
+      const validPassword = await bcrypt.compare(password, user.password)
+
+      if (!validPassword) {
+        return res.status(400).json({ error: "Invalid Password or Password does not match" })
+      }
+
+      const { _id } = user
+
+      console.log(user, "USER")
+      let token = jwt.sign({
+        _id,
+        email,
+        user
+
+      }, process.env.JWT_SECRET)
+
+      console.log(token, "TOKENNNNNN")
+      res.setHeader('Set-Cookie', cookie.serialize('myCookie', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        maxAge: 86400,
+        sameSite: 'strict',
+        path: '/'
+      }));
+      // res.cookie('myCookie', token, { expire: 86400 })
+      res.send({ token, user: { _id, email } })
+    } else {
+      return res.status(400).json({ error: "Invalid request parameters." })
+    }
+  } else if (req.method === "GET") {
+    let users = await User.find()
+
+    if (!users) {
+      return res.status(400).json({ error: "Something went wrong" })
+    }
+
+    res.send(users);
+  } else {
+    res.status(405).json({ error: "Method not allowed" })
+  }
   // catch (error) {
   //   console.error("Error in handler:", error);
   //   res.status(500).json({ error: "Internal server error" })
